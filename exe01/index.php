33ca2b23ebd6c327e896a69b6d69c9b116ed89db
Caio <caiovilarouca@gmail.com>
2026-04-15 11:08:34 -0300

    feat exe01

---
+++ exe01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pr-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exe01</title>
+</head>
+<body>
+    <?= $name = "Caio Vilarouca" ?>
+    <?= "<p>Meu nome é <strong>Caio Vilarouca</strong></p>"?>
+
+    <?php
+        echo "A data atual ".date("d/m/Y");
+        echo "<br>Hora atual ".date("g:i:s");
+    ?>
+</body>
+</html>
