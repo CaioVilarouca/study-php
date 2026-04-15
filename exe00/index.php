0e01679caa86907469f5beb338843b131d9adb89
Caio <caiovilarouca@gmail.com>
2026-04-15 09:41:49 -0300

    feat

---
+++ exe00/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Exe00</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hello, Word! \u{1f30E} :Unicode";
+        ?>
+    </h1>
+    <p>Informações do servidor.</p>
+    <br>
+    <?php 
+        phpinfo(); 
+    ?>
+</body>
+</html>
