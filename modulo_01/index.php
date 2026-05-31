7263b581dd6d7e94834335cccb7bdacf6c466b46
Caio <caiovilarouca@gmail.com>
2026-05-31 16:06:20 -0300

    Modulo_01, Hello, Word!

---
+++ modulo_01/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <title>Módulo 01</title>
+</head>
+<body>
+    <h1>
+        <?= "Olá, Mundo! \u{1F30E}" ?>
+    </h1>
+    <p>Caio Vilarouca</p>
+    <hr>
+    <?= phpinfo() ?>
+</body>
+</html>
