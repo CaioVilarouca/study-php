1508575d4e830574853725973ec0b39b2c5f567b
Caio <caiovilarouca@gmail.com>
2026-05-31 17:48:13 -0300

    modulo_01 coneceitos

--- modulo_01/index.php
+++ modulo_01/index.php
@@ -8,7 +8,12 @@
         <?= "Olá, Mundo! \u{1F30E}" ?>
     </h1>
     <p>Caio Vilarouca</p>
+    <?php 
+        date_default_timezone_set("America/Sao_Paulo"); // GMT -3 horas de Londres
+        echo "Hoje é dia ".date("d/m/yy"). " <br>Horas: " .Date("G:i:s T");
+    ?>
     <hr>
+    <br><br>
     <?= phpinfo() ?>
 </body>
 </html>
