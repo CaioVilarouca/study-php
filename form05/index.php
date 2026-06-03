1361348da1a667b0a87bb0fab7ac66856704750b
Caio <caiovilarouca@gmail.com>
2026-06-03 19:49:45 -0300

    form05 (conversor de moedas usando API para pegar automaticamente a cotação do dólar pelo Banco Central)

--- form05/index.php
+++ form05/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>form 04</title>
+    <title>form 05</title>
 </head>
 <body>
     <h1>Conversor de Moedas</h1>
@@ -13,15 +13,30 @@
         <input type="submit" value="Converter">
     </form>
     <?php
+    ?>
+    <pre>
+    <?php // conversor de moedas usando API para pegar automaticamente a cotação do dólar pelo Banco Central
+        $inicio = date("m-d-Y", strtotime("-6 days"));
+        $fim = date("m-d-Y");
+        
+        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+        $dados = json_decode(file_get_contents($url) ,true);
+        //var_dump($dados);
+
+        $cotacao = $dados["value"][0]["cotacaoCompra"];
+
         $money = $_GET["money"] ?? 0;
         // Formatação de moedas com internacionalização
         // Biblioteca intl (Internallization PHP)
+
         $standard = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
         if ($money != 0) {
             printf("Seus <strong>%s</strong> equivalem a <strong>%s</strong>",
             numfmt_format_currency($standard, $money, "BRL"),
-            numfmt_format_currency($standard, $money/5.02, "USD"));
+            numfmt_format_currency($standard, $money/$cotacao , "USD"));
         }
     ?>
+</pre>
 </body>
 </html>
